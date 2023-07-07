558041272c29ee3f0e736f5ba1cc03466e9b8668
al-desktop <finlay.woolfson@gmail.com>
2023-07-07 11:09:16 +0100

    add comments

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { useState } from "react"
 
 function App() {
 
+  //start with the default settings
   var defaultsettings = {
     number: 1
   }
 
+  //initialise the settings, and the unsaved settings
   const [settings, setSettings] = useState(defaultsettings);
   const [presettings, setPreSettings] = useState(defaultsettings);
 
+  //when the number of images is changed, remember this in the unsaved settings
   const saveUserNumber = function (event) {
     setPreSettings({
       ...presettings,
@@ -17,21 +20,23 @@ function App() {
     })
   }
 
+  //when the button is pushed, we want to update the real settings with the unsavedsettings
   const generateImages = function () {
     setSettings({
       ...presettings
     })
   };
 
-  var divs = [];
+  //here, we want to use the real settings to generate our output
+  var images = [];
   for(var i = 0; i < settings.number; i++)
-    divs.push(<div className="box"></div>);
+    images.push(<div className="box"></div>);
 
   return (
     <div>
       <input type="number" defaultValue={defaultsettings.number} onChange={saveUserNumber}></input>
       <button onClick={generateImages}>Generate Images</button>
-      {divs}
+      {images}
     </div>
   );
 }
